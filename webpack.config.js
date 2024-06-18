5d8d2049ff7cb819a10c7c608a0c5780b6e1f0af
Pavel <pavelpalonko@gmail.com>
2024-06-18 15:16:02 +0300

    added webpack config file, cache file, dynamic filename

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+  // місце входу, звідки почнеться збірка
+  // є можливість додати кілька точок входу - { piont: path.resolve(...) }
+  entry: path.resolve(__dirname, "src", "index.js"),
+
+  // місце в яке буде проведено збірку
+  output: {
+    path: path.resolve(__dirname, "build"),
+
+    // [name], [contenthash] - динамічна назва файлу та хеш, в залежносіт від змісту файлу
+    filename: "[name].[contenthash].js",
+
+    // очистка папки "build" перед збіркою.
+    clean: true,
+  },
+};
